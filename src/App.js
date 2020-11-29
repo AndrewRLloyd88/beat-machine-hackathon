dd3ffe159eb3b460dbd567daf80e41e4fde135bf
Yimeng Yu <yimeng.yu@gmail.com>
2020-11-29 10:01:00 -0800

    2 sounds play together

--- src/App.js
+++ src/App.js
@@ -6,24 +6,38 @@ import BeatMachine from "./Components/BeatMachine"
 import InstrumentRow from './Components/InstrumentRow';
 import Bpm from "./helpers/useBPM"
 import BeatTracker from "./Components/BeatTracker"
+import sounds from "./song.json"
 
 const App = () => {
 const beats = Bpm();
   const playSound = () => {
     var sound = new Howl({
-      src: ["./DrumSamples/MainSnare/Snare1.wav"],
+      src: [sounds[0].source],
       html5: true,
     });    
     sound.play();
     Howler.volume(1);
   }
-  
+  const playSound1 = () => {
+    var sound = new Howl({
+      src: [sounds[1].source],
+      html5: true,
+    });    
+    sound.play();
+    Howler.volume(1);
+  }
+  const playTogether = () => {
+    playSound()
+    playSound1()
+  }
     return(
       <>
       <BeatMachine />
         <h1>Hello World, Let's try a pull request!</h1>
         <h2>{beats} per minute</h2>
+        <button onClick={playTogether}>press</button>
         <button onClick={playSound}>press</button>
+        <button onClick={playSound1}>press</button>
 
         <table border='2'>
       
