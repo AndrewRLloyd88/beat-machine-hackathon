50c6515c79466394bcb5d1f5a67e9a72d1741e09
Edward Smith <edward.m.smith80@gmail.com>
2020-11-28 20:52:55 -0700

    add table with toggle for each cell

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './styles/index.css';
 import {Howl, Howler} from 'howler';
 import BeatMachine from "./Components/BeatMachine"
-// import airplane from './assets/airplane-landing_daniel_simion.mp3';
-
-
-
-// let path = './assets/airplane-landing_daniel_simion.mp3';
-
-// audioPlayer = (
-  //             <div className="count-number-equivelance-audioPlayer">
-  //                 <button id="equivalence-audio-button" onClick={() => audio.play()}>Soundclip!</button>
-  //             </div>
-  //         ); 
-  
-// let audio = new Audio(airplane);
+import InstrumentRow from './Components/InstrumentRow';
 
 const App = () => {
   
-  // useEffect(() => {
-  //   audio.load();
-  // }, []);
-
-
   const playSound = () => {
     console.log("clicked")
     var sound = new Howl({
@@ -35,13 +18,22 @@ const App = () => {
     Howler.volume(1);
   }
   
-  // This sound file may not work due to cross-origin setting
-
     return(
       <>
       <BeatMachine />
         <h1>Hello World, Let's try a pull request!</h1>
         <button onClick={playSound}>press</button>
+
+        <table border='2'>
+          <tbody>
+           <InstrumentRow />
+           <InstrumentRow />
+           <InstrumentRow />
+           <InstrumentRow />
+           <InstrumentRow />
+           <InstrumentRow />
+         </tbody>
+        </table>
       </>
     )
   }
